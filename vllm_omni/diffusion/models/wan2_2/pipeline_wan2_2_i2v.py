94d70bd9b490a6259037183b1c2156e334681ef7
LyxWxj <936543814@qq.com>
2026-07-01 21:26:38 +0800

    fix bug: Wan22 add dummy diffusion request id

--- vllm_omni/diffusion/models/wan2_2/pipeline_wan2_2_i2v.py
+++ vllm_omni/diffusion/models/wan2_2/pipeline_wan2_2_i2v.py
@@ -42,7 +42,7 @@ from vllm_omni.diffusion.models.wan2_2.pipeline_wan2_2 import (
 from vllm_omni.diffusion.models.wan2_2.wan2_2_transformer import WanTransformer3DModel
 from vllm_omni.diffusion.postprocess import interpolate_video_tensor
 from vllm_omni.diffusion.profiler.diffusion_pipeline_profiler import DiffusionPipelineProfilerMixin
-from vllm_omni.diffusion.request import OmniDiffusionRequest
+from vllm_omni.diffusion.request import DUMMY_DIFFUSION_REQUEST_ID, OmniDiffusionRequest
 from vllm_omni.inputs.data import OmniDiffusionSamplingParams, OmniTextPrompt, OmniTokensPrompt
 from vllm_omni.platforms import current_omni_platform
 
